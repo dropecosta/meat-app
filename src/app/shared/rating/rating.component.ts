6ff8911d1b131cc174439ae18675ebd9fa1aef5b
Pedro Costa <dropecosta@gmail.com>
2018-09-09 06:31:33 -0300

    implement rating component

--- src/app/shared/rating/rating.component.ts
+++ src/app/shared/rating/rating.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'mt-rating',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class RatingComponent implements OnInit {
+
+  @Output() rated = new EventEmitter<number>()
 
   rates: number[] = [1,2,3,4,5]
 
@@ -21,12 +23,14 @@ export class RatingComponent implements OnInit {
   setRate(r: number){
     this.rate = r
     this.previusRate = undefined
+    this.rated.emit(this.rate)
   }
 
   setTemporaryRate(r: number){
     if (this.previusRate === undefined){
       this.previusRate = this.rate
     }
+    this.rate = r
   }
 
   clearTemporaryRate(r: number){
