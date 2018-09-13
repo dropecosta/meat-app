27362e9a60cd531f87edeaa328be0b43bf0b193b
dropecosta <dropecosta@gmail.com>
2018-09-13 18:01:49 -0300

    refatoring application with HttpClient

--- src/app/order/order.model.ts
+++ src/app/order/order.model.ts
@@ -4,8 +4,10 @@ class Order {
       public number: number,
       public optionalAddress: string,
       public paymentOption: string,
-      public orderItems: OrderItem[] = []
+      public orderItems: OrderItem[] = [],
+      public id?: string
     ){}
+
   }
   
   class OrderItem {
