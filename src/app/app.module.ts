e312558398d305b932659558283924803d81af67
dropecosta <pedro-reis@puc.br>
2018-10-01 11:13:11 -0300

    adding login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,8 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { OrderSummaryComponent } from './order-summary/order-summary.component'
 
 import { SharedModule } from './shared/shared.module';
-import { NotFoundComponent } from './not-found/not-found.component'
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './security/login/login.component'
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import { NotFoundComponent } from './not-found/not-found.component'
     MenuItemComponent,
     ReviewsComponent,
     OrderSummaryComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
