363d9cff737c0ecd4b41ade448a9cdd1071ad9ab
dropecosta <pedro-reis@puc.br>
2018-10-03 11:30:43 -0300

    adding http interceptors to headers on request

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -16,8 +16,7 @@ export class OrderService {
 
     constructor(
         private cartService: ShoppingCartService, 
-        private http: HttpClient,
-        private loginService: LoginService) {}
+        private http: HttpClient) {}
 
     itemsValue(): number{
         return this.cartService.total()
@@ -44,11 +43,7 @@ export class OrderService {
     }
 
     checkOrder(order: Order): Observable<string> {
-        let headers = new HttpHeaders()
-        if(this.loginService.isLoggedIn()){
-            headers = headers.set('Authorization', `Bearer ${this.loginService.user.accessToken}`)
-        }
-        return this.http.post<Order>(`${MEAT_API}/orders`, order, {headers:headers})
+        return this.http.post<Order>(`${MEAT_API}/orders`, order)
                         .map(order => order.id)
     }
 
